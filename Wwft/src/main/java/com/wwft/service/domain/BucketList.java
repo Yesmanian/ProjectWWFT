2c16522eba512e92b6c33485618582d0de3468ef
ralios123 <ralios@naver.com>
2021-01-15 13:58:48 +0900

    shin :  commit!!@

---
+++ Wwft/src/main/java/com/wwft/service/domain/BucketList.java
@@ -0,0 +1,69 @@
+package com.wwft.service.domain;
+
+import java.sql.Date;
+
+public class BucketList {
+	
+	private int bucketListNo;
+	private String bucketListWriter;
+	private String bucketListDetail;
+	private Date bucketListRegDate;
+	private String stampState;
+	private int treeNo;
+	
+	public BucketList() {
+		
+	}
+	
+	public int getBucketListNo() {
+		return bucketListNo;
+	}
+	public void setBucketListNo(int bucketListNo) {
+		this.bucketListNo = bucketListNo;
+	}
+	public String getBucketListWriter() {
+		return bucketListWriter;
+	}
+	public void setBucketListWriter(String bucketListWriter) {
+		this.bucketListWriter = bucketListWriter;
+	}
+	public String getBucketListDetail() {
+		return bucketListDetail;
+	}
+	public void setBucketListDetail(String bucketListDetail) {
+		this.bucketListDetail = bucketListDetail;
+	}
+	public Date getBucketListRegDate() {
+		return bucketListRegDate;
+	}
+	public void setBucketListRegDate(Date bucketListRegDate) {
+		this.bucketListRegDate = bucketListRegDate;
+	}
+	public String getStampState() {
+		return stampState;
+	}
+	public void setStampState(String stampState) {
+		this.stampState = stampState;
+	}
+	public int getTreeNo() {
+		return treeNo;
+	}
+	public void setTreeNo(int treeNo) {
+		this.treeNo = treeNo;
+	}
+
+	@Override
+	public String toString() {
+		return "BucketList [bucketListNo=" + bucketListNo + ", bucketListWriter=" + bucketListWriter
+				+ ", bucketListDetail=" + bucketListDetail + ", bucketListRegDate=" + bucketListRegDate
+				+ ", stampState=" + stampState + ", treeNo=" + treeNo + "]";
+	}
+}	
+	
+	
+	
+	
+	
+	
+	
+
--- Wwft/src/main/java/com/wwft/service/tree/impl/TreeDaoImpl.java
+++ Wwft/src/main/java/com/wwft/service/tree/impl/TreeDaoImpl.java
@@ -1,11 +1,149 @@
 package com.wwft.service.tree.impl;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
+
+import com.wwft.service.domain.Calender;
+import com.wwft.service.domain.Forest;
+import com.wwft.service.domain.Profile;
+import com.wwft.service.domain.Tree;
 import com.wwft.service.tree.TreeDao;
 
+@Repository("treeDaoImpl")
 public class TreeDaoImpl implements TreeDao {
 
+	
+	@Autowired
+	@Qualifier("sqlSessionTemplate")
+	private SqlSession sqlSession;
+	public void setSqlSession(SqlSession sqlSession) {
+		System.out.println("::"+getClass()+".sqlSession() Call..");
+		this.sqlSession = sqlSession;
+	}
+	
+	
 	public TreeDaoImpl() {
 		// TODO Auto-generated constructor stub
 	}
 
+
+	@Override
+	public Tree getTree(int treeNo) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public List<Profile> getProfileIntroduce() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public Tree getTreeRanking(int treeNo) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public Tree getWorldTreeRanking(int treeNo) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public Tree getNationTreeRanking(int treeNo) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public List<Forest> getForestList() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public Tree getTreeGrowth(int treeNo) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public Tree getBucketList(int treeNo) throws Exception {
+		return sqlSession.selectOne("BucketListMapper.getBucketList", treeNo);
+	}
+
+
+	@Override
+	public void addBucketList(Tree tree) throws Exception {
+		sqlSession.insert("BucketListMapper.addBucketList", tree);
+		
+	}
+
+
+	@Override
+	public void updateBucketList(Tree tree) throws Exception {
+		sqlSession.update("BucketListMapper.updateBucketList", tree);
+		
+	}
+
+
+	@Override
+	public void removeBucketList(Tree tree) throws Exception {
+		sqlSession.delete("BucketListMapper.removeBucketList", tree);
+		
+	}
+
+
+	@Override
+	public Calender getCalender(int treeNo) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public void searchTreeAndForest() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public Tree getNoticeMessageBox(int treeNo) throws Exception {
+		return null;
+	}
+
+
+	@Override
+	public void addFamilyMotto(Tree tree) throws Exception {
+		sqlSession.insert("TreeMapper.addFamilyMotto", tree);
+		
+	}
+
+
+	@Override
+	public void updateFamilyMotto(Tree tree) throws Exception {
+		sqlSession.update("TreeMapper.updateFamilyMotto", tree);
+		
+	}
+
+
+	@Override
+	public void removeFamilyMotto(Tree tree) throws Exception {
+		sqlSession.delete("TreeMapper.removeFamilyMotto", tree);
+
+	}
 }
