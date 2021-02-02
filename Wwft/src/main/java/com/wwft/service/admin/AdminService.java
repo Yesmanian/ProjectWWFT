07df448dc86a817117670dae4d6d582a556d9348
cjdcns91 <1004iyun@gmail.com>
2021-02-02 11:06:01 +0900

    ian : commit 02/02

--- Wwft/src/main/java/com/wwft/service/admin/AdminService.java
+++ Wwft/src/main/java/com/wwft/service/admin/AdminService.java
@@ -1,8 +1,13 @@
 package com.wwft.service.admin;
 
+import java.util.Map;
+
+import com.wwft.common.web.Search;
 import com.wwft.service.domain.Report;
 
 public interface AdminService {
 	
 	public void addReport(Report report) throws Exception;
+	
+	public Map<String, Object> getReportList(Search search) throws Exception;
 }
