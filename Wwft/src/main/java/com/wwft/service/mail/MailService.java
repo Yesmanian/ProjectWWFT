85c70647432add8e3fec6618a0c7883c0512ebfa
shwls <shwls@192.168.1.24>
2021-02-14 17:32:47 +0900

    no : mail confilct 해결

--- Wwft/src/main/java/com/wwft/service/mail/MailService.java
+++ Wwft/src/main/java/com/wwft/service/mail/MailService.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.wwft.service.mail;
 
 public interface MailService {
@@ -15,21 +14,3 @@ public interface MailService {
 
 
 }
-=======
-package com.wwft.service.mail;
-
-public interface MailService {
-	
-    /** 메일 전송
-     *  @param subject 제목
-     *  @param text 내용
-     *  @param from 보내는 메일 주소
-     *  @param to 받는 메일 주소
-     *  @param filePath 첨부 파일 경로: 첨부파일 없을시 null **/
-    public boolean send(String subject, String text, String from, String to, String filePath)throws Exception;
- 
-
-
-
-}
->>>>>>> refs/heads/master
